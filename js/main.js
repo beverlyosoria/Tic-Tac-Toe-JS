0cbc7c3f3c2eb8df3ccc41944a21751bbbe7534e
beverlyosoria <bevosoria@gmail.com>
2019-07-28 16:58:17 -0500

    Adds constants

--- js/main.js
+++ js/main.js
@@ -1,5 +1,23 @@
 /*----- constants -----*/
+const COLORS = {
+  "0": null,
+  "1": "X",
+  "-1": "O"
+};
+
 /*----- app's state (variables) -----*/
+let board, turn, winner; //naming variables in the global scope
+
 /*----- cached element references -----*/
 /*----- event listeners -----*/
 /*----- functions -----*/
+
+function init() {
+  board = [
+    [0, 0, 0], //column 1 (index 0)
+    [0, 0, 0], //column 2 (index 1)
+    [0, 0, 0] //column 3 (index 2)
+  ];
+  turn = 1;
+  winner = null; // 1, -1, null (no winner), T (tie)
+}
