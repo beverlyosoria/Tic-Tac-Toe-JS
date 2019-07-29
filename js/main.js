cc83236c7cf782eb581a42822045feb7e16f4651
beverlyosoria <bevosoria@gmail.com>
2019-07-28 20:02:38 -0500

    Updates handleClick function

--- js/main.js
+++ js/main.js
@@ -32,25 +32,25 @@ function init() {
 }
 
 function render() {
-  board.forEach(function (colArr, colIdx) { // render the board
-    console.log(colArr);
+  // board.forEach(function (colArr, colIdx) { // render the board
+  //   console.log(colArr);
 
-    colArr.forEach(function (cell, rowIdx) {
-      let div = document.getElementById(`c${colIdx}r${rowIdx}`);
-      div.textContent = TURNS[cell];
+  //   colArr.forEach(function (cell, rowIdx) {
+  //     let div = document.getElementById(`c${colIdx}r${rowIdx}`);
+  //     div.textContent = TURNS[cell];
 
-    });
-  });
+  // });
+  // });
 }
 
-function handleClick(evt) {
-  let idx = parseInt(evt.target.id.replace("col", "")); //get index of column's box clicked
-  if (isNaN(idx) || winner) return; //check if box was clicked/if there's a winner
-  let colArr = board[idx]; //obtain the column array in board array
-  let rowIdx = colArr.indexOf(0); // get the index of the first 0 in the col array
-  if (rowIdx === -1) return; //if the col is full, there are no zeros, indexOf returns -1
-  // if no zeroes available (col full), do nothing
-  colArr[rowIdx] = turn; //update the col array (within the board) with the player whos turn it is
-  turn *= -1; // flip turns from 1 to -1; -1 to 1
-  // winner = getWinner(); //update the winner
+function handleClick(event) {
+  let box = event.target;
+  if (turn === 1) {
+    box.textContent = "X";
+  } else {
+    box.textContent = "O";
+  }
+  turn *= -1;
+  render();
+
 }
